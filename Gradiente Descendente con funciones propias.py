5c2706211598e2563be21ff885fad82a97129eda
kathytowerz <89817165+kathytowerz@users.noreply.github.com>
2021-10-26 11:59:28 -0500

    Update Gradiente Descendente con funciones propias.py

--- Gradiente Descendente con funciones propias.py
+++ Gradiente Descendente con funciones propias.py
@@ -11,22 +11,48 @@ Usar funciones propias para el cálculo del producto de matrices y
 la transpuesta de matrices.
 
 """
-A = [[2.0, 1.0, -3.0], [5.0, -4.0, 1.0], [1.0, -1.0, -4.0]]
-print(A)
+A_coef = [[2.0, 1.0, -3.0], [5.0, -4.0, 1.0], [1.0, -1.0, -4.0]]
+b_coef = [7.0, -19.0, 4.0]
+x_sol = [1.0, 1.0, 1.0]
+print(A_coef)
+print(b_coef)
 def transpuesta (M):
     height = len(M)
     width = len(M[0])
 #Se itera fila por fila y las devuelve en forma de columnas:
     M_t = [[M[row][col] for row in range(0,height)] for col in range(0,width)]
     return M_t
-print(transpuesta(A))
+print(transpuesta(A_coef))
+#print(transpuesta(b_coef))
+def M_Matricial (A,b):
+    C=[]
+    for i in range(len(A)):
+        for j in range(len(b)):
+            x=0
+            for n in range(len(A[0])):
+                x=x+A[i][n]*b[n]
+        C.append(x)
+    return C
 
+def gradient(x, A, b):
+	element_1 = M_Matricial(transpuesta(A), M_Matricial(A, x))
+	element_2 = M_Matricial(transpuesta(A), b)
+	return element_1 - element_2
+
+def linear_solve(M, v, x_start, umbral, max_iter):
+    k = 0.002
+    for i in range(max_iter):
+        print(x_start)
+        x_start = x_start - k * gradient(x_start, M, v)
+        current_v = M_Matricial(M,x_start)
+        error = (abs(current_v-v))
+        if error < umbral:
+            return x_start
 '''
 def gradient(x, A, b):
 	element_1 = np.dot(np.transpose(A),np.dot(A, x))
 	element_2 = np.dot(np.transpose(A), b)
 	return element_1 - element_2
-
 def linear_solve(M, v, x_start, umbral, max_iter):
     k = 0.002
     for i in range(max_iter):
@@ -39,7 +65,6 @@ def linear_solve(M, v, x_start, umbral, max_iter):
     #return x_start
     
 #La multiplicación matricial en forma de lista
-
 def M_Matricial (A,b):
     C=[]
     for i in range(len(A)):
@@ -50,6 +75,5 @@ def M_Matricial (A,b):
         C.append(x)
     return C
 	
-
-print(linear_solve(A_coef, b_coef, x_sol, 0.001, 10000))
 '''
+print(linear_solve(A_coef, b_coef, x_sol, 0.001, 10000))
